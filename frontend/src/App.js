b32ed7c4233c26c2d77444a345bba88329dcef0e
nikowis <nikowis@gmail.com>
2020-01-12 12:16:26 +0100

    Sass support, minor style top menu change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import './App.scss';
 import Login from "./components/Login";
 import CreateGoal from "./components/CreateGoal";
 import FulfilGoal from "./components/FulfilGoal";
--- frontend/src/components/TopMenu.js
+++ frontend/src/components/TopMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './../App.css';
+import '../App.scss';
 import {Link} from "react-router-dom";
 
 
@@ -21,13 +21,15 @@ class TopMenu extends React.Component {
     headerContent() {
 
         return (<div>
-                <nav>
+                <nav className="navigation">
                     {this.props.login ? null : <Link to="/login">Login</Link>}
                     {this.props.login ? <Link to="/home">Home</Link> : null}
                     {this.props.login ? <Link to="/create">Create</Link> : null}
                     {this.props.login ? <Link to="/fulfil">Fulfil</Link> : null}
-                    {this.props.login ? "Welcome " + this.props.login : null}
                 </nav>
+                <div className="welcome-message">
+                    {this.props.login ? "Welcome " + this.props.login : null}
+                </div>
             </div>
         );
     }
