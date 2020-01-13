d46e1d0fc05c0e899ec116b767f1b7dea3f23698
nikowis <nikowis@gmail.com>
2020-01-13 22:37:25 +0100

    Create react env files.

--- api/src/main/java/pl/nikowis/selfcare/SelfCareApplication.java
+++ api/src/main/java/pl/nikowis/selfcare/SelfCareApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-
 public class SelfCareApplication extends SpringBootServletInitializer {
 
 	@Bean
--- frontend/src/common/api-communication.js
+++ frontend/src/common/api-communication.js
@@ -1,24 +1,31 @@
-const Api = {
-    createGoal: function (goal) {
-        return fetch('http://localhost:8080/goals', {
+import {API_GOALS} from './constants'
+
+class Api {
+
+    constructor() {
+        this.API_URL = process.env.REACT_APP_API_URL;
+    }
+
+    createGoal(goal) {
+        return fetch(this.API_URL + API_GOALS, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(goal)
         })
-        .then((response) => {
-            return response.json();
-        })
-        .catch(console.log)
-    },
+            .then((response) => {
+                return response.json();
+            })
+            .catch(console.log)
+    };
 
 
-    getGoals: function (login) {
-        return fetch('http://localhost:8080/goals')
+    getGoals(login) {
+        return fetch(this.API_URL + API_GOALS)
             .then(res => res.json())
             .catch(console.log)
-    }
-};
+    };
+}
 
-export default Api;
+export default Api = new Api();
