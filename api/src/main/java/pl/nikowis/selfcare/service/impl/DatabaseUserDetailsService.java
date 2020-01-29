483ecbe06cbeadd925ce4eacb101eaa0bade8aaf
nikowis <nikowis@gmail.com>
2020-01-29 23:12:39 +0100

    Fixed login issue

--- api/src/main/java/pl/nikowis/selfcare/service/impl/DatabaseUserDetailsService.java
+++ api/src/main/java/pl/nikowis/selfcare/service/impl/DatabaseUserDetailsService.java
@@ -5,10 +5,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.nikowis.selfcare.model.User;
 import pl.nikowis.selfcare.service.UserService;
 
 @Service
+@Transactional
 public class DatabaseUserDetailsService implements UserDetailsService {
 
     @Autowired
