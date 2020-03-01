c380806f9072a7dc0b2f8d9e31de9c16eebe363f
nikowis <nikowis@gmail.com>
2020-03-01 01:05:50 +0100

    Boostrap: error container

--- frontend/src/components/ErrorContainer.js
+++ frontend/src/components/ErrorContainer.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import '../App.scss';
+import Alert from "react-bootstrap/Alert";
 import {connect} from "react-redux";
 
 class ErrorContainer extends React.Component {
 
+    errorContainer = (message) => {
+        return (
+            <Alert variant='danger'>
+                {message}
+            </Alert>
+        );
+    };
+
     render() {
         return (
             <div className='error-container'>
-                {this.props.authError ? "Authentication error" : null}
-                {this.props.apiError ? "Api error" : null}
+                {this.props.authError ? this.errorContainer("Authentication error") : null}
+                {this.props.apiError ? this.errorContainer("Api error") : null}
             </div>
         );
     }
-
 }
 
-export default connect( state => ({
+export default connect(state => ({
     authError: state.app.authError,
     apiError: state.app.apiError
 }))(ErrorContainer);
